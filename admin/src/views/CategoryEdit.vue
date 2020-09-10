b85707ab6252515dc8f8bb8b8909c4d157e02513
Len <532904508@qq.com>
2020-09-11 00:37:58 +0800

    连接数据库、数据库传值

--- admin/src/views/CategoryEdit.vue
+++ admin/src/views/CategoryEdit.vue
@@ -21,8 +21,13 @@ export default {
     }
   },
   methods: {
-    save(){
-      // this.$http.post();
+    async save(){
+      const res = await this.$http.post('categories',this.model);// eslint-disable-line
+      this.$router.push('/categories/list')
+      this.$message({
+        type:'success',
+        message:'保存成功'
+      })
     }
   },
 }
